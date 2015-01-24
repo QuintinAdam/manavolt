05174a5ae7015ef9c0f05a6c5880f4637575397b
Quintin Adam <QuintinAdam@live.com>
2015-01-24 09:52:10 -0700

    procstuff

--- config/app.rb
+++ config/app.rb
@@ -4,15 +4,20 @@ Volt.configure do |config|
   # Your app secret is used for signing things like the user cookie so it can't
   # be tampered with.  A random value is generated on new projects that will work
   # without the need to customize.  Make sure this value doesn't leave your server.
-  config.app_secret = 'vpFH5019mC6kGUmzzGc_lFZcoUoW9ScxWHwtDg82ckd1jbRiOTXBgeTBINkZBAR7Cww'
+  config.app_secret = ENV['SECRET_TOKEN'] || 'vpFH5019mC6kGUmzzGc_lFZcoUoW9ScxWHwtDg82ckd1jbRiOTXBgeTBINkZBAR7Cww'
 
   # Database config all start with db_ and can be set either in the config
   # file or with an environment variable (DB_NAME for example).
 
-  # config.db_driver = 'mongo'
-  # config.db_name = (config.app_name + '_' + Volt.env.to_s)
-  # config.db_host = 'localhost'
-  # config.db_port = 27017
+  config.db_driver = 'mongo'
+  config.db_name = (config.app_name + '_' + Volt.env.to_s)
+
+  if ENV['MONGOHQ_URL'].present?
+    config.db_uri = ENV['MONGOHQ_URL']
+  else
+    config.db_host = 'localhost'
+    config.db_port = 27017
+  end
 
   # Compression options
 
